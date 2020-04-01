cbb4e913d2838b37cee541a3024c8f55faf7b883
rafaelcalixto <rafaelufrj@yahoo.com.br>
2020-03-31 22:21:39 -0300

    Adding functions to create Private and Public keys

--- sign.go
+++ sign.go
@@ -4,6 +4,10 @@ import (
     "flag"
     "crypto/rand"
     "crypto/rsa"
+    "crypto/x509"
+    "encoding/pem"
+    "errors"
+    "fmt"
 )
 
 // Genetare RSA Keys
@@ -11,16 +15,50 @@ func GenerateKeys() (*rsa.PrivateKey, *rsa.PublicKey) {
     privatekey, err := rsa.GenerateKey(rand.Reader, 2048)
     if err != nil { fmt.Println(err.Error) }
 
-    return privatekey, privatekey.PublicKey
+    return privatekey, &privatekey.PublicKey
 }
+
+func Keys2String(privKey *rsa.PrivateKey, publKey *rsa.PublicKey) (string, string) {
+    priv_bytes := x509.MarshalPKCS1PrivateKey(privKey)
+    priv_pem := pem.EncodeToMemory(
+        &pem.Block{
+            Type  : "RSA PRIVATE KEY",
+            Bytes : priv_bytes,
+        },
+    )
+
+    publ_bytes, err := x509.MarshalPKIXPublicKey(publKey)
+    if err != nil { errors.New("Fail to covert the public key to string") }
+
+    publ_pem := pem.EncodeToMemory(
+        &pem.Block{
+            Type  : "RSA PUBLIC KEY",
+            Bytes : publ_bytes,
+        },
+    )
+    return string(priv_pem), string(publ_pem)
+}
+
+// func String2Keys2String() ()
 
 func main() {
     mode := flag.Bool("v", false, "a bool")
     createkeys := flag.Bool("generate", false, "a bool")
-    filename := flag.Args()[0]
     flag.Parse()
+    filename := flag.Args()
+
 
     // Getting keys
     privatekey, publicky := GenerateKeys()
 
+    // Converting Keys to String
+    priv_key_str, pub_key_str := Keys2String(privatekey, publicky)
+
+    fmt.Println(priv_key_str, pub_key_str)
+    fmt.Println("mode: ", *mode)
+    fmt.Println(*createkeys)
+    fmt.Println(filename)
+    if len(filename) > 0 {
+        fmt.Println(filename)
+    }
 }
